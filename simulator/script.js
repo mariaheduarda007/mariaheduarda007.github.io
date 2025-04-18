81f45584393433f4f3179458ea6d5f08baf62c7c
MariaEduarda-git <mariah.bsc18@gmail.com>
2025-04-18 14:40:24 -0300

    Faltando as propriedades flex

---
+++ simulator/script.js
@@ -0,0 +1,68 @@
+let colorsArray = [
+  "#pink-box",
+  "#blue-box",
+  "#salmon-box",
+  "#white-box",
+  "#green-box",
+  "#orange-box",
+];
+let formatsArray = ["#format-round", "#format-square"];
+
+function selectColor(color) {
+  for (let i = 0; i < colorsArray.length; i++) {
+    const colorBox = document.querySelector(colorsArray[i]);
+    colorBox.classList.remove("box-selection");
+  }
+
+  const colorBox = document.querySelector(color);
+  colorBox.classList.add("box-selection");
+
+  const flexDivs = document.querySelectorAll(".flex-box");
+  const colorStyle =
+    getComputedStyle(colorBox).getPropertyValue("background-color");
+  flexDivs.forEach((div) => {
+    div.style.setProperty("background-color", colorStyle);
+  });
+}
+
+function selectFormat(format) {
+  for (let i = 0; i < formatsArray.length; i++) {
+    const formatBox = document.querySelector(formatsArray[i]);
+    formatBox.classList.remove("box-selection");
+  }
+
+  const formatBox = document.querySelector(format);
+  formatBox.classList.add("box-selection");
+
+  const flexDivs = document.querySelectorAll(".flex-box");
+  const formatStyle =
+    getComputedStyle(formatBox).getPropertyValue("border-radius");
+
+  if (formatStyle.match("50%")) {
+    flexDivs.forEach((div) => {
+      div.style.setProperty("border-radius", formatStyle);
+    });
+  } else {
+    flexDivs.forEach((div) => {
+      div.style.setProperty("border-radius", "0px");
+    });
+  }
+}
+
+function inputSize() {
+    
+    const inputBox = document.querySelector("#size-options");
+    const flexDivs = document.querySelectorAll(".flex-box");
+    
+    const value = Number(inputBox.value);
+    
+        if (Number(inputBox.value) <= 180 && Number(inputBox.value) >= 100){
+            
+           flexDivs.forEach((div) => {
+             div.style.setProperty("width", value + "px");
+             div.style.setProperty("height", value + "px");
+           });
+           
+        }
+        
+}
