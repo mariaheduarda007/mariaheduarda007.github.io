966e2e91cf25b66b85906962f60f947140b286d8
MariaEduarda-git <mariah.bsc18@gmail.com>
2025-05-11 15:58:35 -0300

    Final

--- space/script.js
+++ space/script.js
@@ -1,5 +1,4 @@
-let counter;
-let counterText;
+let counter = 0;
 let val = 0;
 let hr = 0;
 let min = 0;
@@ -7,38 +6,23 @@ let sec = 0;
 hrStr = "00";
 minStr = "00";
 secStr = "00";
-let display = "";
 function add() {
   val++;
-  sec = val;
 
-  if (val > 3600) {
-    hr = Math.floor(min / 60);
-  }
+  hr = Math.floor(val / 3600);
+  min = Math.floor(val / 60);
+  sec = val % 60;
 
-  if (val > 60) {
-    min = Math.floor(val / 60);
-    sec = val - min * 60;
+  hrStr = hr < 10 ? "0" + hr : "" + hr;
+  minStr = min < 10 ? "0" + min : "" + min;
+  secStr = sec < 10 ? "0" + sec : "" + sec;
 
-    if (min >= 10) {
-      minStr = min;
-    } else {
-      minStr = "0" + min;
-    }
-  }
-
-  if (sec >= 10) {
-    secStr = sec;
-  } else {
-    secStr = "0" + sec;
-  }
-
-  display = "00:" + minStr + ":" + secStr;
-
-  document.querySelector("#counter").textContent = display;
+  document.querySelector("#counter").textContent =
+    hrStr + minStr + ":" + secStr;
 }
 
 function startCounter() {
+  stopCounter();
   counter = setInterval(add, 1000);
 }
 
@@ -50,22 +34,23 @@ document.addEventListener("keyup", function (e) {
   let modal = document.querySelector("#modal");
   if ((e.key === "p" || e.key === "P") && modal.style.display === "none") {
     modal.style.display = "flex";
+    stopCounter();
   } else {
+    startCounter();
     modal.style.display = "none";
   }
 });
 
 document.addEventListener("keydown", function (e) {
-    let leftLimit = 200;
-    let rightLimit = 1400;
+  let leftLimit = 200;
+  let rightLimit = 1400;
 
   let nave = document.querySelector("#nave");
   let left = parseInt(getComputedStyle(nave).left);
   let right = left - 20;
   if (e.key === "ArrowRight") {
-    if (left < rightLimit){
-
-        nave.style.left = left + 20 + "px";
+    if (left < rightLimit) {
+      nave.style.left = left + 20 + "px";
     }
   }
   if (e.key === "ArrowLeft") {
