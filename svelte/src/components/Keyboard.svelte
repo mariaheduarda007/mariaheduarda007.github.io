a0abf78a53804c1ae2a464ca165a0d15e2a0ddbd
MariaEduarda-git <mariah.bsc18@gmail.com>
2025-06-08 10:20:30 -0300

    Final

--- svelte/src/components/Keyboard.svelte
+++ svelte/src/components/Keyboard.svelte
@@ -1,41 +1,77 @@
 <script>
   export let display = "";
-  let expression = ""; // SER nUMERO IR FAZENDO CONTA DINAMICANTE
+  let opArray = [];
   let cleanForm = false;
-  let init = true
+  let init = true;
+
+  const isNumber = (value) => !isNaN(Number(value));
+  const sumOp = (a, b) => Number(a) + Number(b);
+  const subtractOp = (a, b) => Number(a) - Number(b);
+  const divideOp = (a, b) => Number(a) / Number(b);
+  const multiplyOp = (a, b) => Number(a) * Number(b);
 
   function digit(d) {
-  if(init){
-    display = "";
-  }
-  init = false
-    if (typeof d === "number") {
+    if (init) {
+      display = "";
+    }
+    init = false;
+    if (isNumber(d)) {
       if (cleanForm) {
         display = d;
         cleanForm = false;
       } else {
         display += d.toString();
       }
-      expression += d;
     } else if (d === "C") {
+      opArray.length = 0;
       display = "0";
-      expression = "";
-      init = true
-    } else if (d === "=") {
-      alert(calcExpression(expression));
-      expression = "";
-      display = "0";
-      init = true 
 
-    } else if (typeof d !== "number") {
+      init = true;
+    } else if (d === "=") {
+      opArray.push(display);
+      calculate();
+      display = opArray[0]
+      opArray.length = 0;
+    } else if (!isNumber(d)) {
+      if (!cleanForm) {
+        opArray.push(display);
+      }
+      if (!isNumber(opArray[opArray.length - 1])) {
+        opArray.pop();
+      }
       cleanForm = true;
-      expression += d;
+
       display = d;
+      opArray.push(display);
     }
   }
 
-  function calcExpression(expression) {
-    return expression;
+  function calculate() {
+    while (opArray.length > 1) {
+      for (let i = 0; i < opArray.length; i++) {
+        if (opArray[i] == "*") {
+          opArray[i - 1] = multiplyOp(opArray[i - 1], opArray[i + 1]);
+          opArray.splice(i, 2);
+          i--;
+        } else if (opArray[i] == "/") {
+          opArray[i - 1] = divideOp(opArray[i - 1], opArray[i + 1]);
+          opArray.splice(i, 2);
+          i--;
+        }
+      }
+      for (let i = 0; i < opArray.length; i++) {
+        if (opArray[i] == "+") {
+          opArray[i - 1] = sumOp(opArray[i - 1], opArray[i + 1]);
+          opArray.splice(i, 2);
+          i--;
+        } else if (opArray[i] == "-") {
+          opArray[i - 1] = subtractOp(opArray[i - 1], opArray[i + 1]);
+          opArray.splice(i, 2);
+          i--;
+        }
+      }
+    }
+
   }
 </script>
 
@@ -69,18 +105,6 @@
     justify-content: center;
     align-items: center;
     column-gap: 10px;
-  }
-
-  .number {
-    width: 54px;
-    height: 54px;
-    background-color: black;
-    color: white;
-    font-size: 22px;
-    font-family: "Franklin Gothic Medium", "Arial Narrow", Arial, sans-serif;
-    line-height: 50%;
-    margin: 0;
-    border-radius: 10px;
   }
 
   button {
