450ae09be662f74f3d894340aa634f6a8ce9e9e1
MariaEduarda-git <mariah.bsc18@gmail.com>
2025-06-29 14:27:47 -0300

    Tarefa

---
+++ 2º BIMESTRE/react-tarefa/src/api/index.js
@@ -0,0 +1,8 @@
+const USERAPI ="https://randomuser.me/api/"
+
+
+export {
+    USERAPI,
+} 
+
+// EXPORTA UMA CONSTANTE PRA QUE OUTROS ARQUIVOS POSSAM UTILIZA-LA
---
+++ 2º BIMESTRE/react-tarefa/src/pages/Home/index.js
@@ -0,0 +1,80 @@
+import { Component } from "react";
+//biblioteca do react que permite criar components
+import { USERAPI } from "../../api"
+//importando vaiavel do link da api
+import './styles.css';
+
+export default class Home extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      users: [],
+      load: true,
+      error: null,
+    }
+
+  }
+  componentDidMount() {
+    this.loadUsers();
+  }
+
+  async loadUsers() { // RETORNA UM OBJETO PROMISE, QUE SABE AVISAR QUANDO TERMINOU
+    try {
+      const apiResponse = await fetch(USERAPI + "/?results=10");
+      if (!apiResponse.ok) {
+        throw new Error('ERRO - RESPONSE!')
+      }
+      const json = await apiResponse.json();
+      this.setState({ users: json.results })
+    } catch (e) {
+      this.setState({ error: e })
+    } finally {
+      this.setState({ load: false })
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {
+          this.state.load ?
+            <p>Carregando...</p>
+            :
+            this.state.error ?
+              <p>Erro: {this.error.message}</p>
+              :
+              <div className="card-box">
+                {
+                  this.state.users.map((item) => (
+                    <div className="card" key={item.id}>
+                      <div className="card-photo">
+                        <img className="image"
+                          src={item.picture.large}/>
+                      </div>
+                      <div className="card-item name">
+                        <p>{item.name.title}.
+                          {item.name.last.toUpperCase()} {item.name.first}</p>
+                      </div>
+                      <div className="card-item mail">
+                        <p>{item.dob.age} anos</p>
+                      </div>
+                      <div className="card-item">
+                        <p>{item.location.state}
+                          ({item.location.country})</p>
+                      </div>
+                      <div className="card-item">
+                        <p>{item.location.street.name},
+                          {item.location.street.number}</p>
+                      </div>
+                    </div>
+                  ))
+                }
+              </div>
+        }
+      </div>
+    );
+  }
+
+}
